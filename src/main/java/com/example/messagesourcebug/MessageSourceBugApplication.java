9dd8188cec54f5655ac93ab7c06f6b795b7d0775
Eryk Mariankowski <dagi12@o2.pl>
2019-03-15 13:04:20 +0100

    first commit

---
+++ src/main/java/com/example/messagesourcebug/MessageSourceBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagesourcebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSourceBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSourceBugApplication.class, args);
+	}
+
+}
